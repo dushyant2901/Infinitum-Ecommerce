046942a413521c3397772d90c42e1055e1d4d519
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-02-12 23:36:22 +0530

    add index page in pages directory

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
-
+import { Routes,Route } from 'react-router-dom';
+import {Home,SingleProduct,Products,Error,About,Cart} from './pages'
 function App() {
   return (
-     <></>
+     <>
+     <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='/about' element={<About/>}/>
+       <Route path='/products' element={<Products/>}/>
+       <Route path='/products:id' element={<SingleProduct/>}/>
+       <Route path='/cart' element={<Cart/>}/>
+       <Route path='*' element={<Error/>}/>
+     </Routes>
+     </>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+
+export {About,Cart,Checkout,Error,Home,Products,SingleProduct}
