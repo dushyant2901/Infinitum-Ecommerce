5cccf6f4d361d21b67232d402faf8088bc0507f1
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-02-14 22:23:13 +0530

    Setup product context

---
+++ src/context/products_context.js
@@ -0,0 +1,28 @@
+import React, { useContext, useReducer } from "react";
+import { SIDEBAR_CLOSE, SIDEBAR_OPEN } from "../actions";
+import reducer from '../reducers/products_reducer'
+const ProductsContext = React.createContext();
+const initialstate = {
+    isOpen:false
+};
+export const ProductsProvider = ({ children }) => {
+
+const openSidebar=()=>{
+dispatch({type:SIDEBAR_OPEN})
+}
+const closeSidebar=()=>{
+    console.log('first')
+dispatch({type:SIDEBAR_CLOSE})
+}
+
+
+
+    const [state, dispatch] = useReducer(reducer, initialstate);
+    return (
+        <ProductsContext.Provider value={{...state,openSidebar,closeSidebar}}>{children}</ProductsContext.Provider>
+    );
+};
+
+export const useProductsContext = () => {
+    return useContext(ProductsContext);
+};
