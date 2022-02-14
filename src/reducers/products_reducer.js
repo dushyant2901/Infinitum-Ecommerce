d5f0c5157e9d34b50824c8851a726520e05fc5c5
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-02-14 23:02:08 +0530

    fixed sidebar functionality bug

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -3,10 +3,10 @@ import { SIDEBAR_CLOSE, SIDEBAR_OPEN } from '../actions'
 
 const products_reducer = (state,action) => {
     if(action.type===SIDEBAR_OPEN){
-        return state={...state,isOpen:true}
+        return state={...state,isSidebarOpen:true}
     }
     if(action.type===SIDEBAR_CLOSE){
-        return state={...state,isOpen:false}
+        return state={...state,isSidebarOpen:false}
     }
   return state
     
