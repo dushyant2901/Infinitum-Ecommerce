a2d6b44cc883871d13a68e7d61e871faa52f3560
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-02-14 22:30:04 +0530

    fixed sidebar toggle bug

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useProductsContext } from "../context/products_context";
 const CartButtons = () => {
   const {closeSidebar} = useProductsContext();
 
